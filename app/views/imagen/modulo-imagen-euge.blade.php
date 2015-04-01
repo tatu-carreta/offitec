53197268700dfb95de701d1e70ff0f35199247cc
max-ang <max.angletti@gmail.com>
2015-04-01 09:58:48 -0300

    Subiendo el repositorio

---
+++ app/views/imagen/modulo-imagen-euge.blade.php
@@ -0,0 +1,25 @@
+<script src="{{URL::to('js/moduleImageCrop.js')}}"></script>
+<style type="text/css">
+    #target {
+        background-color: #ccc;
+        width: 500px;
+        height: 330px;
+        font-size: 24px;
+        display: block;
+    }
+
+
+</style>
+
+<div class="marginBottom">
+    <input id="imagen" type="file" name="imagen_portada" onChange="validar(this);" required="true">
+</div>
+
+<div class="divCargaImg">
+    <img id="cropbox" style="width: auto; max-height: 600px;">
+</div>
+<input class="block anchoTotal marginBottom" type="text" name="epigrafe_imagen_portada" placeholder="Ingrese una descripción de la foto">
+<input type="hidden" id="x" name="x">
+<input type="hidden" id="y" name="y">
+<input type="hidden" id="w" name="w">
+<input type="hidden" id="h" name="h">
---
+++ app/views/noticia/agregar.blade.php
@@ -0,0 +1,70 @@
+@extends($project_name.'-master')
+
+@section('contenido')
+    @if (Session::has('mensaje'))
+    <script src="{{URL::to('js/divAlertaFuncs.js')}}"></script>
+    @endif
+    <script src="{{URL::to('js/ckeditorLimitado.js')}}"></script>
+    
+    <section class="container">
+        @if (Session::has('mensaje'))
+            <div class="divAlerta error alert-success">{{ Session::get('mensaje') }}<i onclick="" class="cerrarDivAlerta fa fa-times fa-lg"></i></div>
+        @endif
+        {{ Form::open(array('url' => 'admin/noticia/agregar', 'files' => true)) }}
+            <h2 class="marginBottom2"><span>Carga y modificación de noticias</span></h2>
+            <div id="error" class="error" style="display:none"><span></span></div>
+            <div id="correcto" class="correcto ok" style="display:none"><span></span></div>
+
+            <!-- Abre columna de descripción -->
+            <div class="col70Admin datosProducto">
+                <h3>Título de la noticia</h3>
+                <input class="block anchoTotal marginBottom" type="text" name="titulo" placeholder="Título" required="true" maxlength="100">
+
+                <h3>Fecha</h3>
+                <input class="block anchoTotal marginBottom" type="text" name="fecha" placeholder="Fecha" required="true" maxlength="50">
+                
+                <h3>Fuente</h3>
+                <input class="block anchoTotal marginBottom" type="text" name="fuente" placeholder="Fuente" required="true" maxlength="50">
+
+                <h3>Descripción</h3>
+                <input class="block anchoTotal marginBottom" type="text" name="descripcion" placeholder="Descripción" required="true" maxlength="200">
+
+                <h3>Cuerpo</h3>
+                <div class="divEditorTxt marginBottom2">
+                    <textarea id="texto" contenteditable="true" name="cuerpo"></textarea>
+                </div>
+                
+                <h3>Secciones</h3>
+                @foreach($menu_dinamico as $menu)
+                    @if($menu->modulo()->nombre == $seccion->menuSeccion()->modulo()->nombre)
+                        <h5>{{$menu->nombre}}</h5>
+                        
+                        @foreach($menu->secciones as $seccion)
+                            <input type="checkbox" name="secciones[]" value="{{$seccion->id}}" @if($seccion->id == $seccion_id) checked="true" disabled @endif>@if($seccion->titulo != ""){{$seccion->titulo}}@else Sección {{$seccion->id}} @endif
+                        @endforeach
+                    @endif
+                @endforeach
+                
+            </div>
+
+            <!-- Abre columna de imágenes -->
+            <div class="col30Admin fondoDestacado padding1 cargaImg">
+                <h3>Imagen principal</h3>
+                @include('imagen.modulo-imagen-noticia-maxi')
+            </div>
+
+            <div class="clear"></div>
+            <!-- cierran columnas -->
+            
+            
+
+            <div class="punteado">
+                <input type="submit" value="Publicar" class="btn marginRight5">
+                <a onclick="window.history.back();" class="btnGris">Cancelar</a>
+            </div>
+
+
+            {{Form::hidden('seccion_id', $seccion_id)}}
+        {{Form::close()}}
+    </section>
+@stop
