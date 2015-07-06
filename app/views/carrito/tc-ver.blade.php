b1acc5741baa701ace3ce73db7b3db44cde121d5
carolab167 <carolab167@hotmail.com>
2015-07-06 19:52:06 -0300

    carrito
    tabla y formulario

--- app/views/carrito/tc-ver.blade.php
+++ app/views/carrito/tc-ver.blade.php
@@ -9,8 +9,91 @@
     @if (Session::has('mensaje'))
     <div class="divAlerta ok alert-success">{{ Session::get('mensaje') }}<i onclick="" class="cerrarDivAlerta fa fa-times fa-lg"></i></div>
     @endif
-    <div>
-        <table>
+    
+    <div class="container">
+        <div class="row">
+             <div class="col-md-12 marginBottom2">
+                <h2 class="pull-left">Presupuesto</h2>
+                <div class="redes pull-right">
+                    <a class="facebook" href="#"></a>
+                    <a class="google" href="#"></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class=" col-carrito">
+                    <h3>Productos seleccionados</h3>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td> <img src="imagen1.jpg" alt=""></td>
+                                <td> COD: CXR-324</td>
+                                <td> <input type="" class="form-control"></td>
+                                <td> <a href=""><i class="fa fa-times fa-2x"></i></a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class=" col-carrito">
+                    <div class="borde">
+                        <h3>Complete sus datos</h3>
+                    </div>
+                        <form role="form">
+                            <div class="form-group">
+                                <label for="ejemplo_email_1">Nombre y apellido</label>
+                                <input type="email" class="form-control" id="ejemplo_email_1"
+                                placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="ejemplo_password_1">Empresa</label>
+                                <input type="password" class="form-control" id="ejemplo_password_1" 
+                                placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="ejemplo_password_1">email</label>
+                                <input type="password" class="form-control" id="ejemplo_password_1" 
+                                placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="ejemplo_password_1">Teléfono</label>
+                                <input type="password" class="form-control" id="ejemplo_password_1" 
+                                placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="ejemplo_password_1">Comentarios</label>
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default">Enviar</button>
+                        </form>
+
+                </div>
+            </div>
+        </div>
+       <div class="row">
+            <div class="col-md-6">
+                <div class="info">
+                    <h4>Offitec en La Plata</h4>
+                    <p>Calle 39 N° 833 e/ 11 y 12<br>
+                    Teléfono: (0221) 4221273 / Fax: (0221) 4273777<br>
+                    Email: ventas@offitec.com</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="info">
+                    <h4>Offitec en Lomas de Zamora</h4>
+                    <p>Av. Hipólito Yrigoyen 9275 (ex Av. Pavón)<br>
+                    Teléfono: (011) 4244 4099<br>
+                    Email: lomas@offitec.com</p>
+                </div>
+            </div>
+        </div>
+    <table>
+           
+</div>          
+            <!--
             <tr>
                 <th></th>
                 <th>Producto</th>
@@ -34,7 +117,8 @@
                     <td>${{Cart::total()}}</td>
                 </tr>
             @endif
-        </table>
-    </div>
+        </table> 
+        -->
+   
 </section>
 @stop
