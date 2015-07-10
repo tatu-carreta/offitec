e1d259f875869b80f9c399f67bc83e322e9689d3
carolab167 <carolab167@hotmail.com>
2015-07-10 16:27:54 -0300

    carrousel

--- app/views/slide/slide-estatico-offitec.blade.php
+++ app/views/slide/slide-estatico-offitec.blade.php
@@ -31,7 +31,8 @@
             </div>
         </div>
 
-        <!-- FLECHITAS SLIDER-->
+        <!--
+        
         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Anterior</span>
@@ -40,26 +41,35 @@
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Siguiente</span>
         </a>
+        -->
     </div>
 
 </div><!-- cierra S L I D E estático --> 
 
 <!--C A R O U S E L de colores-->
-<div class="contenedor-carousel">
-    <div class="carousel-home">
-        <div id="owl-demo2">
-            <div class="item"><a class="boton-naranja" href="#">muebles<br>gerenciales</a></div>
-            <div class="item"><a class="boton-rojo" href="#">muebles<br>operativos</a></div>
-            <div class="item"><a class="boton-violeta" href="#">mesas <br> de reunión</a></div>
-            <div class="item"><a class="boton-azul" href="#">mesas <br>bajas</a></div>
-            <div class="item"><a class="boton-verde" href="#">muebles <br> de guardado</a></div>
-            <div class="item"><a class="boton-naranja" href="#">asientos<br>gerenciales</a></div>
-            <div class="item"><a class="boton-rojo" href="#">asientos<br>operativos</a></div>
-            <div class="item"><a class="boton-violeta" href="#">sillas <br>de visita</a></div>
-            <div class="item"><a class="boton-azul" href="#">sillones <br>de visita</a></div>
-            <div class="item"><a class="boton-verde" href="#">cortinas<br> a medida</a></div>
-            <div class="item"><a class="boton-naranja" href="#">accesorios <br>y complementos</a></div>
-            <div class="item"><a class="boton-rojo" href="#">deco <br>casa</a></div>
-        </div>
+<div class="fondo-negro">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contenedor-carousel">
+                    <div class="carousel-home">
+                        <div id="owl-demo2">
+                            <div class="item"><a class="boton-naranja" href="#">muebles<br>gerenciales</a></div>
+                            <div class="item"><a class="boton-rojo" href="#">muebles<br>operativos</a></div>
+                            <div class="item"><a class="boton-violeta" href="#">mesas <br> de reunión</a></div>
+                            <div class="item"><a class="boton-azul" href="#">mesas <br>bajas</a></div>
+                            <div class="item"><a class="boton-verde" href="#">muebles <br> de guardado</a></div>
+                            <div class="item"><a class="boton-naranja" href="#">asientos<br>gerenciales</a></div>
+                            <div class="item"><a class="boton-rojo" href="#">asientos<br>operativos</a></div>
+                            <div class="item"><a class="boton-violeta" href="#">sillas <br>de visita</a></div>
+                            <div class="item"><a class="boton-azul" href="#">sillones <br>de visita</a></div>
+                            <div class="item"><a class="boton-verde" href="#">cortinas<br> a medida</a></div>
+                            <div class="item"><a class="boton-naranja" href="#">accesorios <br>y complementos</a></div>
+                            <div class="item"><a class="boton-rojo" href="#">deco <br>casa</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
     </div>
-</div>    
+</div>
