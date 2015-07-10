f76e8505045341f920c793ff7e12ef01bf745575
eugeMolinero <euge_molinero@yahoo.com.ar>
2015-07-10 17:25:57 -0300

    Arreglos

--- app/views/tc-contacto.blade.php
+++ app/views/tc-contacto.blade.php
@@ -1,28 +1,30 @@
 @extends($project_name.'-master')
 
 @section('contenido')
-<section>
-    <div>
-        <h2>Envíenos su consulta:</h2>
-        <div class="colForm">
-            {{Form::open(array('url' => 'consulta'))}}
-                <label>Nombre y apellido<br>
-                    <input name="nombre" type="text" required>
-                </label>
-                <label>Teléfono<br>	
-                    <input name="telefono" type="text" required>
-                </label>
-                <label>E-mail<br>
-                    <input name="email" type="email" required>
-                </label>
-                <label>Consulta<br>
-                    <textarea name="consulta"></textarea>
-                </label>
-                <input type="submit" value="consultar">
-            {{Form::close()}}
+<section class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Envíenos su consulta:</h2>
+            <div class="colForm">
+                {{Form::open(array('url' => 'consulta'))}}
+                    <label>Nombre y apellido<br>
+                        <input class="form-control" name="nombre" type="text" required>
+                    </label>
+                    <label>Teléfono<br>	
+                        <input class="form-control" name="telefono" type="text" required>
+                    </label>
+                    <label>E-mail<br>
+                        <input class="form-control" name="email" type="email" required>
+                    </label>
+                    <label>Consulta<br>
+                        <textarea class="form-control" name="consulta"></textarea>
+                    </label>
+                    <input class="btn btn-primary" type="submit" value="Consultar">
+                {{Form::close()}}
+                <div class="clear"></div>
+            </div>
             <div class="clear"></div>
         </div>
-        <div class="clear"></div>
     </div>
 </section>
 @stop
