7cd25422bd113747b6351bcc0d2bf6a69347e238
max-ang <max.angletti@gmail.com>
2015-07-20 12:21:56 -0300

    Arreglos vista secciones en agregar/editar prod

--- app/views/producto/agregar.blade.php
+++ app/views/producto/agregar.blade.php
@@ -99,14 +99,12 @@
                         @foreach($menues as $men)
                         <div class="cadaSeccion">
                             @if(count($men->children) == 0)
-                            
-                                <div><label for="menu{{$men->id}}">{{$men->nombre}}</label></div>
                                 <div>
-                                @foreach($men->secciones as $seccion)
-                                    <span><input id="menu{{$men->id}}" type="checkbox" name="secciones[]" value="{{$seccion->id}}" @if($seccion->id == $seccion_id) checked="true" disabled @endif>{{-- @if($seccion->titulo != ""){{$seccion->titulo}}@else Sección {{$seccion->id}} @endif --}}</span>
-                                @endforeach
+                                    @foreach($men->secciones as $seccion)
+                                        <span><input id="menu{{$men->id}}" type="checkbox" name="secciones[]" value="{{$seccion->id}}" @if($seccion->id == $seccion_id) checked="true" disabled @endif>{{-- @if($seccion->titulo != ""){{$seccion->titulo}}@else Sección {{$seccion->id}} @endif --}}</span>
+                                    @endforeach
                                 </div>
-                            
+                                <div><label for="menu{{$men->id}}">{{$men->nombre}}</label></div>
                             @endif
                         </div>
                         @endforeach
