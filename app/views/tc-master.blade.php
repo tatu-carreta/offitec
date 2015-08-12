24c949c5b58887e606e0de58528267f8657ae19c
max-ang <max.angletti@gmail.com>
2015-08-12 14:46:44 -0300

    Ventana Carrito

--- app/views/tc-master.blade.php
+++ app/views/tc-master.blade.php
@@ -103,6 +103,22 @@
                             <h1 class="logo pull-left"><a href="{{URL::to('/')}}" ><img alt="logo" src="{{URL::to('images/offitec.svg')}}"></a></h1>
                              <!-- B T N   C A R R I T O -->
                             <a href="{{URL::to('carrito')}}" class="btnCarrito active"><span>Presupuesto: {{Cart::count(false)}}</span><i class="fa fa-shopping-cart fa-lg"></i></a>
+                            
+                            <!-- VENTANA CARRITO -->
+                            @if(Session::has('producto_carrito_subido'))
+                                @if(Session::get('producto_carrito_subido'))
+                                    <!-- ventana Carrito -->
+                                    <div id="ventanaCarrito" class="divEmergente">
+                                        <div class="triang"></div>
+                                        <div class="cerrarEmergente cerrarVentanaCarrito"><i class="fa fa-times-circle-o fa-lg"></i></div>
+                                            <img class="imgArtPedido" src="@if(!is_null(Session::get('producto_carrito')->item()->imagen_destacada())){{ URL::to(Session::get('producto_carrito')->item()->imagen_destacada()->carpeta.Session::get('producto_carrito')->item()->imagen_destacada()->nombre) }}@else{{URL::to('images/sinImg.gif')}}@endif" alt="">
+                                            <p>Agregaste a carrito<br>
+                                            <span>Cod: {{ Session::get('producto_carrito')->item()->titulo }}</span></p>
+                                            <a href="{{URL::to('carrito')}}" class="btn btn-default">Ver Presupuesto</a>
+                                    </div>
+                                @endif
+                            @endif
+                            
                             <!-- N A V -->
                             @include('menu.'.$project_name.'-desplegar-menu')
                             
