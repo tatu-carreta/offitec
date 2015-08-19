b0c3d9943489ee13014eb72f1fb907f711215304
eugeMolinero <euge_molinero@yahoo.com.ar>
2015-08-19 16:00:00 -0300

    Estilos

--- app/views/seccion/tc-ver-seccion.blade.php
+++ app/views/seccion/tc-ver-seccion.blade.php
@@ -1,4 +1,3 @@
-
 <div class="divProductos">
     @if(($seccion->titulo != "") || (Auth::check()))<h3 class="floatLeft marginRight" id="{{$seccion->estado.$seccion->id}}">@if($seccion->titulo != ""){{ $seccion -> titulo }} @endif</h3>@endif
     @if(Auth::check())
